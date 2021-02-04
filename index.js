ea90c4e3af7a8018b4775d02b1a77d88b98dfce5
CesarGP <cgpcsjcmp@gmail.com>
2021-02-04 14:04:42 +0100

    fix: format

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const port  = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/badge', (req, res) => {
-    res.send("Not implemented yet");
+  res.send('Not implemented yet');
 });
 
 http.listen(port, () => {
-    console.log("Server ready on port " +  port);
+  console.log('Server ready on port ' + port);
 });
