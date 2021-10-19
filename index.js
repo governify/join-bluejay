f88fcb7c301ed8b6c48e0825a0dd3d317ef9cef1
alesancor1 <alesancor1@alum.us.es>
2021-10-19 14:32:22 +0200

    feat: local testing

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const app = require('express')();
 const http = require('http').createServer(app);
-const port = process.env.PORT || 5000;
+const port  = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    if (process.env.NODE_ENV === 'production') {
+        res.sendFile(__dirname + '/index.html');
+    } else {
+        res.sendFile(__dirname + '/index-local.html');
+    }
 });
 
 app.get('/badge', (req, res) => {
-  res.send('Not implemented yet');
+    res.send("Not implemented yet");
 });
 
 http.listen(port, () => {
-  console.log('Server ready on port ' + port);
+    console.log("Server ready on port " +  port);
 });
