16e908279f0808724ecbe29dbf6c670d13984a47
borjar20 <borjar20@gmail.com>
2022-01-04 22:24:30 +0100

    feat: Info Wizard

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const port  = process.env.PORT || 5000;
+const dotenv = require('dotenv');
+const axios = require('axios');
+dotenv.config();
+
+var CryptoJS = require("crypto-js");
 
 app.get('/', (req, res) => {
     if (process.env.NODE_ENV === 'production') {
-        res.sendFile(__dirname + '/index.html');
+        res.sendFile(__dirname + '/views/index.html');
     } else {
-        res.sendFile(__dirname + '/index-local.html');
+        res.sendFile(__dirname + '/views/index-local.html');
     }
+});
+
+app.get('/wizard', (req, res) => {
+    res.sendFile(__dirname + '/views/wizard.html');
+});
+
+app.get('/encripted_field_check', (req, res) => {
+    res.sendFile(__dirname + '/views/checker.html');
+});
+
+app.get('/encrypt', (req, res) => {
+    var encrypted = CryptoJS.AES.encrypt(req.query.text, process.env.PRIVATE_KEY);
+    res.send(encrypted.toString())
+});
+
+app.get('/check_encrypt', (req, res) => {
+    var decrypted = CryptoJS.AES.decrypt(req.query.encValue, process.env.PRIVATE_KEY);
+    res.send(decrypted.toString(CryptoJS.enc.Utf8) === req.query.value)
+});
+
+app.get('/getYml', async(req, res) => {
+    let yml 
+    await axios.get('https://raw.githubusercontent.com/' + req.query.githubOwner + '/' + req.query.githubRepo + '/' + 'main' + '/' + 'info.yml', { headers: { Authorization: process.env.KEY_GITHUB ? 'token ' + process.env.KEY_GITHUB : '' }})
+        .then(res => yml = res.data)
+        .catch(error => console.log(error))
+    res.send(yml)
 });
 
 app.get('/badge', (req, res) => {
@@ -17,3 +48,5 @@ app.get('/badge', (req, res) => {
 http.listen(port, () => {
     console.log("Server ready on port " +  port);
 });
+
+
