fd46421fcdc17f046f40612ab53f4d6d4723f8de
Alejandro Santisteban <72552837+alesancor1@users.noreply.github.com>
2023-02-13 00:28:49 +0100

    feat: installed newrelic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+if (process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) require('newrelic');
+
 const app = require('express')();
 const http = require('http').createServer(app);
 const port  = process.env.PORT || 5000;
