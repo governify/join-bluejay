e5d7ca83dab089b80fb939646d2d749ac451cc81
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2024-07-05 11:34:05 +0200

    Revert "wizzard check fix?"
    This reverts commit 829eb374e90a09249d86e660736c1a955352a2e9.

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 if (process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) require('newrelic');
 
-const express = require('express');
-const app = express();
-app.use(express.json());
+const app = require('express')();
 const http = require('http').createServer(app);
-const port = process.env.PORT || 5000;
+const port  = process.env.PORT || 5000;
 const axios = require('axios');
 
 var CryptoJS = require("crypto-js");
@@ -22,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.get('/wizard', (req, res) => {
     if (process.env.NODE_ENV === 'production') {
-        res.sendFile(__dirname + '/views/wizard.html');
+    res.sendFile(__dirname + '/views/wizard.html');
     } else {
         res.sendFile(__dirname + '/views/wizard-local.html');
     }
@@ -42,9 +40,9 @@ app.get('/check_encrypt', (req, res) => {
     res.send(decrypted.toString(CryptoJS.enc.Utf8) === req.query.value)
 });
 
-app.get('/getYml', async (req, res) => {
-    let yml
-    await axios.get('https://raw.githubusercontent.com/' + req.query.githubOwner + '/' + req.query.githubRepo + '/' + 'main' + '/' + 'info.yml', { headers: { Authorization: process.env.KEY_GITHUB ? 'token ' + process.env.KEY_GITHUB : '' } })
+app.get('/getYml', async(req, res) => {
+    let yml 
+    await axios.get('https://raw.githubusercontent.com/' + req.query.githubOwner + '/' + req.query.githubRepo + '/' + 'main' + '/' + 'info.yml', { headers: { Authorization: process.env.KEY_GITHUB ? 'token ' + process.env.KEY_GITHUB : '' }})
         .then(res => yml = res.data)
         .catch(error => console.log(error))
     res.send(yml)
@@ -53,28 +51,9 @@ app.get('/getYml', async (req, res) => {
 app.get('/badge', (req, res) => {
     res.sendFile(__dirname + '/views/badge.html');
 });
-app.post('/wizzard-check', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.data.name);
-    fetch('https://scopes.bluejay.governify.io/api/v1/scopes/development/check', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name: req.body?.name, data: req.body?.data })
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            res.send(data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-});
 
 http.listen(port, () => {
-    console.log("Server ready on http://localhost:" + port);
+    console.log("Server ready on port " +  port);
 });
 
 
